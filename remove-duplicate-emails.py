7e5ab01d7216c7cfafccd1e627a3408cb5ab01bb
Jacob Bankston <jacob.bankston.smile@gmail.com>
2019-07-05 20:41:56 -0500

    Changed the string concatenation into a replace method

--- remove-duplicate-emails.py
+++ remove-duplicate-emails.py
@@ -11,11 +11,14 @@ output_email_string = ""
 with open(raw_emails) as email_list:
     email_string = email_list.read()
 
-for index in range(len(email_string)):
-    if email_string[index] != " " and email_string[index] != "\n":
-        new_email_string += email_string[index]
+email_string = email_string.replace(' ', '')            #implemented instead of a concatenation loop with a new string
+email_string = email_string.replace('\n', '')
 
-email_array = new_email_string.split(",")
+# for index in range(len(email_string)):
+#     if email_string[index] != " " and email_string[index] != "\n":
+#         new_email_string += email_string[index]
+
+email_array = email_string.split(",")
 
 for index in range(len(email_array)):
     if email_array[index] not in new_email_array:
@@ -25,7 +28,6 @@ new_email_array.sort()
 
 for email in new_email_array:
     output_email_string += f'{email}, \n'
-
 
 with open(sorted_emails, "w") as new_email_list:
     new_email_list.write(output_email_string)
